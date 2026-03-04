ca0d927ad43e564cb946951b6cb655a6176bcb7c
Jeremy Cline <jeremycline@linux.microsoft.com>
2026-03-04 14:10:20 -0500

    siguldry-pkcs11: Add test to sign with gnupg-pkcs11-scd
    This tests signing using the PKCS#11 module with gpg2 with the
    gnupg-pkcs11 smart-card daemon.
    
    Signed-off-by: Jeremy Cline <jeremycline@linux.microsoft.com>

--- siguldry-pkcs11/tests/signing.rs
+++ siguldry-pkcs11/tests/signing.rs
@@ -587,6 +587,134 @@ async fn sign_sha512_ecdsa_openssl_provider() -> anyhow::Result<()> {
     Ok(())
 }
 
+// Use gnupg-pkcs11-scd to sign using the pkcs11 module.
+#[tokio::test]
+#[tracing_test::traced_test]
+async fn sign_rsa4k_gnupg_pkcs11_scd_protected_auth() -> anyhow::Result<()> {
+    let instance = InstanceBuilder::new()
+        .auto_unlock_keys()
+        .with_pgp_key()
+        .with_client_proxy()
+        .build()
+        .await?;
+    let data = "🦡🦡🦡🦡🍄🍄".as_bytes();
+    let data_path = instance.state_dir.path().join("data");
+    tokio::fs::write(&data_path, data).await?;
+    let sig_path = instance.state_dir.path().join("data.sig");
+
+    let expected_pubkey = instance
+        .client
+        .get_key(keys::PGP_KEY_NAME.to_string())
+        .await?;
+    let certificate_path = instance.state_dir.path().join("signing_key.asc");
+    let cert = expected_pubkey
+        .openpgp_certificates()
+        .first()
+        .cloned()
+        .unwrap();
+    let _fingerprint = match cert {
+        siguldry::protocol::Certificate::Pgp {
+            version: _,
+            certificate,
+            fingerprint,
+        } => {
+            tokio::fs::write(&certificate_path, certificate.as_bytes()).await?;
+            fingerprint
+        }
+        _ => panic!("was expecting a Pgp cert"),
+    };
+    let gnupg_home = instance.state_dir.path().join("gpghome");
+    tokio::fs::create_dir(&gnupg_home).await?;
+    let gpg_agent_conf = gnupg_home.join("gpg-agent.conf");
+    tokio::fs::write(
+        &gpg_agent_conf,
+        "scdaemon-program /usr/bin/gnupg-pkcs11-scd\n",
+    )
+    .await?;
+
+    let gpg_pkcs11_scd_conf = gnupg_home.join("gnupg-pkcs11-scd.conf");
+    tokio::fs::write(
+        &gpg_pkcs11_scd_conf,
+        format!(
+            "providers siguldry\nprovider-siguldry-library {}\nprovider-siguldry-allow-protected-auth\n",
+            module_path().display()
+        ),
+    )
+    .await?;
+
+    let proxy_path = instance.client_proxy_socket();
+    let output = tokio::process::Command::new("gpg")
+        .env("GNUPGHOME", &gnupg_home)
+        .env("LIBSIGULDRY_PKCS11_PROXY_PATH", &proxy_path)
+        .arg("--batch")
+        .arg("--import")
+        .arg(&certificate_path)
+        .output()
+        .await?;
+    assert!(
+        output.status.success(),
+        "'gpg --import {}' failed:\nstdout: {}\nstderr: {}",
+        certificate_path.display(),
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr),
+    );
+
+    let output = tokio::process::Command::new("gpg")
+        .env("GNUPGHOME", &gnupg_home)
+        .env("LIBSIGULDRY_PKCS11_PROXY_PATH", &proxy_path)
+        .arg("--card-status")
+        .output()
+        .await?;
+    assert!(
+        output.status.success(),
+        "'gpg --card-status' failed:\nstdout: {}\nstderr: {}",
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr),
+    );
+
+    let output = tokio::process::Command::new("gpg")
+        .env("GNUPGHOME", &gnupg_home)
+        .env("LIBSIGULDRY_PKCS11_PROXY_PATH", &proxy_path)
+        .arg("--batch")
+        .arg("--detach-sign")
+        .arg("--output")
+        .arg(&sig_path)
+        .arg(&data_path)
+        .output()
+        .await?;
+    assert!(
+        output.status.success(),
+        "'gpg --detach-sign' failed:\nstdout: {}\nstderr: {}",
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr),
+    );
+
+    let output = tokio::process::Command::new("gpg")
+        .env("GNUPGHOME", &gnupg_home)
+        .arg("--verify")
+        .arg(&sig_path)
+        .arg(&data_path)
+        .output()
+        .await?;
+    assert!(
+        output.status.success(),
+        "'gpg --verify {} {}' failed:\nstdout: {}\nstderr: {}",
+        sig_path.display(),
+        data_path.display(),
+        String::from_utf8_lossy(&output.stdout),
+        String::from_utf8_lossy(&output.stderr),
+    );
+
+    let _ = tokio::process::Command::new("gpgconf")
+        .env("GNUPGHOME", &gnupg_home)
+        .arg("--kill")
+        .arg("gpg-agent")
+        .output()
+        .await;
+    instance.halt().await?;
+    Ok(())
+}
+
 // Use Sequoia's cryptoki backend to sign using the pkcs11 module.
 //
 // Note: This test only works with a version of sequoia that supports
@@ -612,7 +740,7 @@ async fn sign_rsa4k_via_sequoia() -> anyhow::Result<()> {
         .get_key(keys::PGP_KEY_NAME.to_string())
         .await?;
     let certificate_path = instance.state_dir.path().join("signing_key.asc");
-    let cert = expected_pubkey.certificates.first().unwrap();
+    let cert = expected_pubkey.certificates.first().cloned().unwrap();
     let fingerprint = match cert {
         siguldry::protocol::Certificate::Pgp {
             version: _,
